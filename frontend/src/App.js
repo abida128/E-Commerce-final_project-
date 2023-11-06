5d6f3c33352e76a8c583a85e87ff6f7e178c68a6
aanamarshad <FA19-BSE-084@cuilahore.edu.pk>
2023-11-07 03:22:41 +0500

    contactPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import './App.css';
 import {
   BrowserRouter,
   Routes,
@@ -10,6 +12,7 @@ import {
 import LoginPage from "./pages/login/login";
 import ForgotPass from "./pages/login/forgotPass";
 import SignPage from "./pages/signup/signup";
+import ContactPage from './pages/ContactPage.js';
 import { SuspenseLayout } from "./layouts/SuspenseLayout";
 import { ErrorBoundary } from "./components/errorBoundary";
 import { AppRouter } from "./routes";
