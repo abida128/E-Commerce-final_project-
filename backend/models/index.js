170447399613f6c6591d8ccb0b89a576b9f7d048
rashidkarim11 <rashidkarim35.75@gmail.com>
2023-11-10 12:26:37 +0500

    add : cart api

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,4 +2,5 @@ module.exports = {
   User: require("./UserModel"),
   Categories: require("./categoriesModel"),
   Product: require("./productModel"),
+  ShoppingCart: require("./shoppingCartModel"),
 };
