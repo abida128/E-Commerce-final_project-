e9a95a4c6483edbf18861e7d8c250a6d6291edac
abida128 <abida.nizam11@gmail.com>
2023-11-20 14:09:40 +0500

    updated

--- frontend/src/components/Home/RatingCard.js
+++ frontend/src/components/Home/RatingCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaRegStar } from "react-icons/fa";
+// import { FaRegStar } from "react-icons/fa";
 
 const RatingCard = ({ imageSrc, name, detail, price, maxWidth, marginTop }) => {
   return (
@@ -9,15 +9,7 @@ const RatingCard = ({ imageSrc, name, detail, price, maxWidth, marginTop }) => {
       <img src={imageSrc} alt="fashion" />
       <span className="mt-4 block">{name}</span>
       <h2 className="font-semibold mt-2 font-poppins ">{detail}</h2>
-      <div className="text-center">
-        {/* <i className="FaRegStar flex justify-center">
-        <FaRegStar />
-        <FaRegStar />
-        <FaRegStar />
-        <FaRegStar />
-        <FaRegStar />
-      </i> */}
-      </div>
+      
       <p className="font-bold mt-2">{price}</p>
     </div>
   );
