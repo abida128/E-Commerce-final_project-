b1dcfc41686d1b4b918d3d7df7b9b8eb8b6635cd
rashidkarim11 <rashidkarim35.75@gmail.com>
2023-11-20 16:08:58 +0500

    add : dashboard

--- frontend/src/components/Home/RatingCard.js
+++ frontend/src/components/Home/RatingCard.js
@@ -6,10 +6,14 @@ const RatingCard = ({ imageSrc, name, detail, price, maxWidth, marginTop }) => {
     <div
       className={`text-center text-[#415161] font-poppins lg:text-center ${maxWidth} ${marginTop}`}
     >
-      <img src={imageSrc} alt="fashion" />
+      <img
+        src={imageSrc}
+        alt="fashion"
+        className="min-w-full min-h-[400px]  max-h-[400px] object-cover"
+      />
       <span className="mt-4 block">{name}</span>
       <h2 className="font-semibold mt-2 font-poppins ">{detail}</h2>
-      
+
       <p className="font-bold mt-2">{price}</p>
     </div>
   );
